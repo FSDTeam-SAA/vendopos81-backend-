e9f3b3f074b083c81e54dfd7eddc8db336aad61f
Sajjad177 <sajjadhossainx06@gmail.com>
2026-01-23 06:47:54 +0600

    payment all for admin added

---
+++ src/modules/supplierSettlement/supplierSettlement.interface.ts
@@ -0,0 +1,11 @@
+import { Types } from "mongoose";
+
+export interface ISupplierSettlement {
+  orderId: Types.ObjectId | string;
+  supplierId: Types.ObjectId | string;
+  totalAmount: number;
+  adminCommission: number;
+  payableAmount: number;
+  status: "pending" | "transferred";
+  transferredAt: Date;
+}
